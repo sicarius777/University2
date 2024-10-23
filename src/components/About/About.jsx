68a7c9330b51977fa8856f83d1aea824037bfae0
cameronmcnz <you@example.com>
2024-10-23 10:31:08 -0400

    'new'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,7 +17,7 @@ const About = ({ setPlayState }) => {
       </div>
       <div className="about-right">
         <h3>About</h3>
-        <h2>Your university</h2>
+        <h2>Me but this would be your about section.</h2>
         <p> As a dedicated full-stack developer, I offer a comprehensive range of services tailored to meet the diverse needs of modern businesses and individuals. From front-end design to back-end development, I specialize in creating dynamic and user-friendly web applications that not only look great but also perform seamlessly. My expertise includes HTML, CSS, JavaScript, and popular frameworks such as React and Node.js, ensuring that your website is responsive, efficient, and scalable. Whether you need a simple landing page or a complex web application, I am committed to delivering high-quality solutions that enhance user experience and drive engagement. You can contact me below if you like it is fully functional!</p>
       </div>
     </div>
